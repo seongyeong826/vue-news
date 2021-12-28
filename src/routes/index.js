d044ca2aa27ecef19c5dca3395733ea0633daad3
seongyeong826 <seongyeong826@users.noreply.github.com>
2021-12-29 00:17:28 +0900

    no message

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,18 @@ import VueRouter from "vue-router";
 import NewsView from '../views/NewsView.vue'
 import AskView from '../views/AskView.vue'
 import JobsView from '../views/JobsView.vue'
+import ItemView from '../views/ItemView.vue'
+import UserView from '../views/UserView.vue'
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
+  mode: 'history',
   routes: [
+    {
+      path: '/',
+      redirect: '/news'
+    },
     {
       // path: url 주소
       path: "/news",
@@ -22,5 +29,13 @@ export const router = new VueRouter({
       path: "/jobs",
       component: JobsView,
     },
+    {
+      path: "/item",
+      component: ItemView,
+    },
+    {
+      path: "/user",
+      component: UserView,
+    }
   ],
 });
